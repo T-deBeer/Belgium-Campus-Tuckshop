facb4c48e648c940bf8550f7e4b8bf27da9751b9
Tiaan de Beer <tdebeer.za@gmail.com>
2022-07-30 13:32:06 +0200

    Adding classes

---
+++ Belgium Campus Tuckshop/PersonModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassLibrary
+{
+    public class PersonModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surame { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Belgium Campus Tuckshop/SqliteDataAccess.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class SqliteDataAccess
+    {
+        public static List<PersonModel> LoadPeople()
+        {
+            using (IDbConnection database = new SQLiteConnection(LoadConnectionString()))
+            {
+                var output = database.Query<PersonModel>("SELECT * FROM Cashiers", new DynamicParameters());
+                return output.ToList();
+            }
+        }
+
+
+        private static string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
