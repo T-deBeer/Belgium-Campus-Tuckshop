2e8bc20ae384e8768ea7fe4904982c89536dcb1f
Yandre Bekker <578297@student.belgiumcampus.ac.za>
2022-08-02 18:58:46 +0200

    SalesReports Comments

--- Belgium Campus Tuckshop/SalesReport.cs
+++ Belgium Campus Tuckshop/SalesReport.cs
@@ -19,21 +19,26 @@ namespace Belgium_Campus_Tuckshop
             InitializeComponent();
         }
 
-        private void mbtnDelete_Click(object sender, EventArgs e)
+        private void mbtnDelete_Click(object sender, EventArgs e) //Delete Button for deleting sales
         {
             try
             {
                 DialogResult Result;
 
+                //Display confirmation message before deleting
+
                 Result = MessageBox.Show("Are you sure you want to delete this sale?", "Caution!", MessageBoxButtons.YesNoCancel);
 
                 if (Result == DialogResult.Yes)
                 {
+                    // Gets index, name, and date of sale customer to delete 
+
                     string name = lbxCustomers.GetItemText(lbxCustomers.SelectedIndex);
                     string date = dateTimePicker1.Value.ToString("MM/dd/yyyy"); 
 
+                    //Call the delete function
+
                     ClassLibrary.SqliteDataAccess.DeleteSale(date, name);
-                    lbxCustomers.Items.Remove(lbxCustomers.SelectedIndex);
                 }
             }
             catch (Exception ex)
@@ -48,10 +53,16 @@ namespace Belgium_Campus_Tuckshop
                 lbxCustomers.Items.Clear();
                 rtbxReceipt.Text = "";
 
+                //Loops through the database
+
                 foreach (var saleModel in listSales)
                 {
+                    //Tests if the date in the database is equal to the datetimepicker
+
                     if (saleModel.SaleDate == Date)
                     {
+                        //If the dates are equal, display the customer's name
+
                         lbxCustomers.Items.Add(saleModel.CustomerName);
                     }
                 }
@@ -62,13 +73,17 @@ namespace Belgium_Campus_Tuckshop
             }
         }
 
-        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e) //Display the customers on a certain date
         {
             rtbxReceipt.ResetText();
+
+            //Decompose the datetimepicker to display the day, month and year seperately
 
             int Day = dateTimePicker1.Value.Day;
             int Month = dateTimePicker1.Value.Month;
             int Year = dateTimePicker1.Value.Year;
+
+            //Store datetimepicker in string variable to compare to the database's date
 
             string Date = dateTimePicker1.Value.ToString("MM/dd/yyyy");
 
@@ -76,10 +91,16 @@ namespace Belgium_Campus_Tuckshop
             {
                 List<ClassLibrary.SaleModel> listSales = ClassLibrary.SqliteDataAccess.LoadAllSales();
 
+                //Loops through the database
+
                 foreach (var saleModel in listSales)
                 {
+                    //If the dates are equal, display the customer's name
+
                     if (saleModel.SaleDate == Date)
                     {
+                        //If the dates are equal, display the customer's name
+
                         lbxCustomers.Items.Add(saleModel.CustomerName);
                     }
                 }
@@ -91,6 +112,8 @@ namespace Belgium_Campus_Tuckshop
 
             try
             {
+                //Display the date in the label above the listbox
+
                 lblCustomers.Text = "Customers On " + DetermineMonth(Month) + " " + Day + " " + Year;
             }
             catch (FormatException ex2)
@@ -99,7 +122,7 @@ namespace Belgium_Campus_Tuckshop
             }
         }
 
-        public static string DetermineMonth(int Month)
+        public static string DetermineMonth(int Month) //This method determines the month name with the month number as a parameter
         {
             switch (Month)
             {
@@ -158,19 +181,27 @@ namespace Belgium_Campus_Tuckshop
             }
         }
 
-        private void lbxCustomers_SelectedIndexChanged(object sender, EventArgs e)
+        private void lbxCustomers_SelectedIndexChanged(object sender, EventArgs e) //Display the customer's receipt
         {
             try
             {
                 List<ClassLibrary.SaleModel> listSales = ClassLibrary.SqliteDataAccess.LoadAllSales();
 
+                //Display the recepitent's name in the label above the richeditbox
+
                 lblReceipt.Text = "Receipt for " + lbxCustomers.GetItemText(lbxCustomers.SelectedItem);
                 rtbxReceipt.ResetText();
 
+                //Loops through the database 
+
                 foreach (ClassLibrary.SaleModel saleModel in listSales)
                 {
+                    //Checks if the customer's name in the listbox is equal to the database's customername
+
                     if (lbxCustomers.SelectedItem.ToString() == saleModel.CustomerName)
                     {
+                        //If it is equal, display the customer's receipt
+
                         rtbxReceipt.Text = saleModel.Receipt;
                     }
                 }
@@ -183,7 +214,7 @@ namespace Belgium_Campus_Tuckshop
             }
         }
 
-        private void mbtnAddItem_Click(object sender, EventArgs e)
+        private void mbtnAddItem_Click(object sender, EventArgs e) //Creates a month report listing all the month's sales
         {
             int Month;
             string Year;
@@ -191,6 +222,8 @@ namespace Belgium_Campus_Tuckshop
 
             try
             {
+                //Decomposes the datetimepicker to get the month
+
                 Month = dateTimePicker1.Value.Month;
                 StringMonth = dateTimePicker1.Value.ToString("MM");
                 Year = (dateTimePicker1.Value.Year.ToString());
@@ -203,12 +236,16 @@ namespace Belgium_Campus_Tuckshop
                 rtbxReceipt.Text += "MONTH REPORT FOR " + DetermineMonth(Month).ToUpper();
                 rtbxReceipt.Text += "\n-----------------------------------------";
 
+                //Loops through the database where the month is equal to the datetimepicker
+
                 foreach (var s in salesList)
                 {
                     rtbxReceipt.Text += "\n" + s.CustomerName + "\t\t" + s.SaleDate;
                     rtbxReceipt.Text += "\n-----------------------------------------";
                     amountSales++;
                 }
+
+                //Displays the amount of sales in the month
 
                 rtbxReceipt.Text += "\n-----------------------------------------";
                 rtbxReceipt.Text += "\nAmount of sales during the month: " + amountSales;
@@ -220,12 +257,12 @@ namespace Belgium_Campus_Tuckshop
             }
         }
 
-        private void mbtnBack_Click(object sender, EventArgs e)
+        private void mbtnBack_Click(object sender, EventArgs e) //Returns to the Main Menu of the program
         {
             UserMenu myUserForm = new UserMenu();
             myUserForm.ShowDialog();
 
-            this.Hide();
+            this.Hide(); //Hides current form
         }
     }
 }
